c957a54dab730a75498dcbdfba298cda16f44014
Sereinfy <122258839+Sereinfy@users.noreply.github.com>
2024-12-02 19:07:08 +0800

    Update clash.py

--- readme/clash.py
+++ readme/clash.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 
 def read_file(file_name):
     """读取文件内容，并返回行列表"""
@@ -23,21 +24,42 @@ def convert_line(line):
         # 不符合格式的行返回空字符串
         return ""
 
-def main():
+def main(input_file, output_file):
     # 读取原始数据
-    original_lines = read_file('rules/adblockdnslite.txt')
+    original_lines = read_file(input_file)
+    
+    # 分离数字开头的行和非数字开头的行
+    numeric_lines = [line for line in original_lines if line.strip() and line.strip()[0].isdigit()]
+    non_numeric_lines = [line for line in original_lines if line.strip() and not line.strip()[0].isdigit()]
+    
+    # 对非数字开头的行按字母顺序排序
+    non_numeric_lines.sort(key=lambda x: x.strip().lower())
+    
+    # 合并数字开头的行和排序后的非数字开头的行
+    sorted_lines = numeric_lines + non_numeric_lines
     
     # 转换数据，忽略不符合格式的行
-    converted_lines = [convert_line(line) for line in original_lines if convert_line(line)]
+    converted_lines = [convert_line(line) for line in sorted_lines if convert_line(line)]
     
     # 在输出数据的前一行插入 payload:
     if converted_lines:
         converted_lines.insert(0, "payload:\n")
     
     # 将转换后的数据写入新的文件或覆盖原文件
-    write_file('rules/dnslite_for_clash.yaml', converted_lines)
+    write_file(output_file, converted_lines)
     
-    print("转换完成，结果已保存至 dns_clash.yaml")
+    print(f"转换完成，结果已保存至 {output_file}")
 
 if __name__ == '__main__':
-    main()
+    # 创建解析器对象
+    parser = argparse.ArgumentParser(description="处理adblock文件并转换格式")
+    
+    # 添加输入和输出文件路径的命令行参数
+    parser.add_argument('input_file', help='输入文件路径')
+    parser.add_argument('output_file', help='输出文件路径')
+    
+    # 解析命令行参数
+    args = parser.parse_args()
+    
+    # 调用main函数，传入文件路径
+    main(args.input_file, args.output_file)
